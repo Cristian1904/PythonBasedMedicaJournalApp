0bedba735cf5a87a7673b211940976ab892cdc16
Dinulescu Petre Cristian <cristian.dinulescu1904@gmail.com>
2024-01-15 14:03:03 +0200

    Push jurnal_page_update
    Adaugare scriere in fisier, jurnal activitati zilnice si trimiterea lor catre main.py

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
+import os
+from flask import Flask, render_template, request
 
-from flask import Flask, render_template, url_for
 app = Flask(__name__)
-app.config['SERVER_NAME'] = 'localhost:5000'  # Replace with your preferred server name and port
+app.config['SERVER_NAME'] = 'localhost:5000'
 app.config['APPLICATION_ROOT'] = '/'
-
-def get_url(target):
-   with app.app_context():
-      return url_for(target)
-
-
-
-
-posts = [
-   {
-      'author': 'Cristi',
-      'title': 'Marian',
-      'content':'First post content',
-      'date': 'April 2020, 1999'
-   },
-   {
-      'author': 'Ruxi',
-      'title': 'ION liviu rebreanu',
-      'content': 'Second post content',
-      'date': 'April 2020, 2003'
-   }
-]
 
 @app.route('/')
 @app.route('/home')
 def home():
-   about = get_url('about')
-   jurnal = get_url('jurnal')
-
-   return render_template('home.html', posts = posts, about_link = about, jurnal_link = jurnal)
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-   home = get_url('home')
-   print(home)
-   return render_template('about.html', posts = posts, home_link = home)
+    return render_template('about.html', home_link=home)
 
-@app.route('/jurnal')
+@app.route('/jurnal', methods=['GET', 'POST'])
 def jurnal():
-   jurnal = get_url('jurnal')
-   return render_template('jurnal.html', posts = posts, jurnal_link = jurnal)
+    if request.method == 'POST':
+        selected_date = request.form['selectDate']
+        activities = request.form['activities']
+        meals = request.form['meals']
+        health_status = request.form['health']
+
+        # Obține calea către directorul scriptului curent
+        target_folder = 'jurnal_zilnic'
+
+        file_name = f"DATA_{selected_date}.txt"
+        file_path = os.path.join(target_folder, file_name)
+        
+        # Scrie datele în fișierul text
+        with open(file_path, 'a', encoding='utf-8') as file:
+            file.write(f"Data: {selected_date}\n")
+            file.write(f"Activități fizice: {activities}\n")
+            file.write(f"Mese: {meals}\n")
+            file.write(f"Starea de sănătate: {health_status}\n\n")
+    return render_template('jurnal.html')
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
